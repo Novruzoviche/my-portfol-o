2f3576fe374a9634431b006bd045cb627eeda9d9
Novruzovich <snovruzov@outlook.com>
2022-09-03 23:18:04 +0400

    add__skillsjs__add__skills__bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,10 +30,24 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-/*=============================== MENU SHOW Y HIDDEN====================*/
+/*=============================== ACCORDION SKILLS====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
 
+    for(i = 0; i< skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
 
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 
 /*=============================== MENU SHOW Y HIDDEN====================*/
