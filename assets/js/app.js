f1d8cc2427d48d99dd3e860a8e34e78a29e121b4
Novruzovich <snovruzov@outlook.com>
2022-09-04 20:21:56 +0400

    add__qualificationjs__css

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,8 +50,25 @@ skillsHeader.forEach((el) =>{
 })
 
 
-/*=============================== MENU SHOW Y HIDDEN====================*/
+/*=============================== Qualification tab ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
+tabContents.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__action')
+    })
+})
 
 
 
